62ec3403ad1cb1fef6d173d8309d9d7b106ffe51
AhmedSaadawy10 <saadawyahmed42@gmail.com>
2024-04-04 21:35:27 +0200

    solving first assignment(lessons from 1-10

---
+++ solv1(1-10).py
@@ -0,0 +1,30 @@
+#1
+"""
+this is my first assignment in python with elzero web school.
+I wish i do best in these assignments,
+let's go !
+"""
+
+#================================================================
+#2
+name="Ahmed Saadawy"
+age="30"
+country="Egypt"
+
+#================================================================
+#3
+print('"'+"My name is : "+name+'"')
+print('"'+"My Age is : "+age+'"')
+print('"'+"My country is : "+country+'"')
+
+#================================================================
+#4
+print("_" * 40)
+print('"'+"Hello, My Name is "+ name +" And My Age is "+ age + " And I Live in "+ country +'."')
+
+#================================================================
+#5
+print("_" * 40)
+print(type(name))
+print(type(age))
+print(type(country))
